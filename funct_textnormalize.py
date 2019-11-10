ab64e6dbab71a0172ff4372390f1b35350eabf8e
Bhargava Sai <BHARGAVASAIREDDYP@GMAIL.COM>
2019-11-10 19:04:19 +0530

    Add files via upload

---
+++ funct_textnormalize.py
@@ -0,0 +1,20 @@
+def text_normalization(text):
+    text=str(text).lower() # text to lower case
+    spl_char_text=re.sub(r'[^ a-z]','',text) # removing special characters
+    tokens=nltk.word_tokenize(spl_char_text) # word tokenizing
+    lema=wordnet.WordNetLemmatizer() # intializing lemmatization
+    tags_list=pos_tag(tokens,tagset=None) # parts of speech
+    lema_words=[]   # empty list 
+    for token,pos_token in tags_list:
+        if pos_token.startswith('V'):  # Verb
+            pos_val='v'
+        elif pos_token.startswith('J'): # Adjective
+            pos_val='a'
+        elif pos_token.startswith('R'): # Adverb
+            pos_val='r'
+        else:
+            pos_val='n' # Noun
+        lema_token=lema.lemmatize(token,pos_val) # performing lemmatization
+        lema_words.append(lema_token) # appending the lemmatized token into a list
+    
+    return " ".join(lema_words) # returns the lemmatized tokens as a sentence 
